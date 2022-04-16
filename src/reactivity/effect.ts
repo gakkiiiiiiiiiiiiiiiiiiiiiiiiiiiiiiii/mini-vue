43c3f0ebb6d74daafb443aa59e7d83d17f81a5c3
Allen <857119664@qq.com>
2022-04-16 17:25:59 +0800

    [A]realize stop method in effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,24 @@
-
+import {extend} from '../shared'
 let activeRefect
 class EffectReactive{
   private _fn
-  constructor(fn,private scheduler?) {
+  public active = true
+  public onStop?: () => void
+  public scheduler?: () => void
+  constructor(fn,options={}) {
     this._fn = fn
+    extend(this,options)
     activeRefect = this
   }
   run() {
-    return this._fn()
-    
+    return this._fn()    
+  }
+  stop() {
+    this.onStop && this.onStop()
+    this.active = false
+  }
+  continue() {
+    this.active = true
   }
 }
 const trackMap = new Map()
@@ -25,23 +35,33 @@ export function track(target, key) {
     depsMap.set(key,dep)
   }
 
+  if(!activeRefect) return
+
   dep.add(activeRefect)
 }
 
 export function trigger(target, key) {
   let depsMap = trackMap.get(target)
   let dep = depsMap.get(key)
+  
   for (const _effect of dep) {
     if (_effect.scheduler) {
       _effect.scheduler()
-    } else {     
-      _effect.run()
+    } else {
+      _effect.active && _effect.run()
     }
   }
 }
 
 export function effect(fn, options:any = {}) {
-  const _effect = new EffectReactive(fn,options.scheduler)
+  const _effect = new EffectReactive(fn,options)
+  
   _effect.run()
-  return fn
+  const runner:any = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
+}
+
+export function stop(runner) {
+  runner.effect.stop()
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
 import { reactive } from '../reactive'
-import { effect } from '../effect'
+import { effect,stop } from '../effect'
 describe('effect', () => {
   it('happy path ', () => {
     const user = reactive({
@@ -46,6 +46,36 @@ describe('effect', () => {
     expect(scheduler).not.toBeCalled()
     obj.foo++;
     expect(scheduler).toBeCalledTimes(1)
+  })
 
+  it('stop and continue', () => {
+    const user = reactive({
+      age:8
+    })
+    let nextAge;
+    const runner = effect(() => {
+      nextAge = user.age + 3
+    })
+
+    expect(nextAge).toBe(11)
+    stop(runner)
+    user.age++
+    expect(nextAge).toBe(11)
+    runner.effect.continue()
+    user.age++
+    expect(nextAge).toBe(13)
+  })
+
+  it('onStop', () => {
+    const user = reactive({
+      age:8
+    })
+    const onStop = jest.fn()
+    let nextAge;
+    const runner = effect(() => {
+      nextAge = user.age + 3
+    },{onStop})
+    stop(runner)
+    expect(onStop).toHaveBeenCalledTimes(1)
   })
 })
