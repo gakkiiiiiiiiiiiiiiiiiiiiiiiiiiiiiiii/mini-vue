dfc14bbe731918d5c6c0f6356e18b37e1f62c783
Allen <857119664@qq.com>
2022-06-02 12:00:01 +0800

    [A] add array test in effect.spec

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,81 +1,95 @@
-import { reactive } from '../reactive'
-import { effect,stop } from '../effect'
+import { reactive } from '../reactive';
+import { effect, stop } from '../effect';
 describe('effect', () => {
-  it('happy path ', () => {
-    const user = reactive({
-      age:8
-    })
-    let nextAge;
-    effect(() => {
-      nextAge = user.age + 3
-    })
+	it('happy path ', () => {
+		const user = reactive({
+			age: 8,
+		});
+		let nextAge;
+		effect(() => {
+			nextAge = user.age + 3;
+		});
 
-    expect(nextAge).toBe(11)
-    user.age++
-    expect(nextAge).toBe(12)
-    user.age = user.age+5
-    expect(nextAge).toBe(17)
-    
-  });
+		expect(nextAge).toBe(11);
+		user.age++;
+		expect(nextAge).toBe(12);
+		user.age = user.age + 5;
+		expect(nextAge).toBe(17);
 
-  it('should return runner when call effect', () => {
-    let foo = 1
-    let runner = effect(() => {
-      foo++
-      return 'foo'
-    })
-    expect(foo).toBe(2)
-    let res = runner()
-    expect(foo).toBe(3)
-    expect(res).toBe('foo')
-  });
+		const arr = [1, 2, 3];
+		const arrOb = reactive(arr);
+		let num;
+		effect(() => {
+			num = arrOb[0];
+		});
+		arrOb[0] = 4;
+		expect(num).toBe(4);
+	});
 
-  it('scheduler', () => {
-    let run;
-    let dummy;
-    const obj = reactive({
-      foo:1
-    })
-    const scheduler = jest.fn(() => {
-      run = runner
-    })
-    const runner = effect(() => {
-      dummy = obj.foo
-    }, { scheduler })
-    expect(dummy).toBe(1)
-    expect(scheduler).not.toBeCalled()
-    obj.foo++;
-    expect(scheduler).toBeCalledTimes(1)
-  })
+	it('should return runner when call effect', () => {
+		let foo = 1;
+		let runner = effect(() => {
+			foo++;
+			return 'foo';
+		});
+		expect(foo).toBe(2);
+		let res = runner();
+		expect(foo).toBe(3);
+		expect(res).toBe('foo');
+	});
 
-  it('stop and continue', () => {
-    const user = reactive({
-      age:8
-    })
-    let nextAge;
-    const runner = effect(() => {
-      nextAge = user.age + 3
-    })
+	it('scheduler', () => {
+		let run;
+		let dummy;
+		const obj = reactive({
+			foo: 1,
+		});
+		const scheduler = jest.fn(() => {
+			run = runner;
+		});
+		const runner = effect(
+			() => {
+				dummy = obj.foo;
+			},
+			{ scheduler }
+		);
+		expect(dummy).toBe(1);
+		expect(scheduler).not.toBeCalled();
+		obj.foo++;
+		expect(scheduler).toBeCalledTimes(1);
+	});
 
-    expect(nextAge).toBe(11)
-    stop(runner)
-    user.age = 9
-    expect(nextAge).toBe(11)
-    runner.effect.continue()
-    user.age++
-    expect(nextAge).toBe(13)
-  })
+	it('stop and continue', () => {
+		const user = reactive({
+			age: 8,
+		});
+		let nextAge;
+		const runner = effect(() => {
+			nextAge = user.age + 3;
+		});
 
-  it('onStop', () => {
-    const user = reactive({
-      age:8
-    })
-    const onStop = jest.fn()
-    let nextAge;
-    const runner = effect(() => {
-      nextAge = user.age + 3
-    },{onStop})
-    stop(runner)
-    expect(onStop).toHaveBeenCalledTimes(1)
-  })
-})
+		expect(nextAge).toBe(11);
+		stop(runner);
+		user.age = 9;
+		expect(nextAge).toBe(11);
+		runner.effect.continue();
+		user.age++;
+		expect(nextAge).toBe(13);
+	});
+
+	it('onStop', () => {
+		const user = reactive({
+			age: 8,
+		});
+		const onStop = jest.fn();
+		let nextAge;
+		const runner = effect(
+			() => {
+				nextAge = user.age + 3;
+			},
+			{ onStop }
+		);
+		stop(runner);
+		expect(onStop).toHaveBeenCalledTimes(1);
+	});
+});
--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,31 +1,31 @@
-import {reactive,readonly,isReactive,isProxy} from '../reactive'
+import { reactive, readonly, isReactive, isProxy } from '../reactive';
 describe('reactive', () => {
-  it('happy path', () => {
-    const origin = { foo: 1 }
-    const observed = reactive(origin)
-    expect(observed).not.toBe(origin)
-    expect(observed.foo).toBe(1)
-  })
+	it('happy path', () => {
+		const origin = { foo: 1 };
+		const observed = reactive(origin);
+		expect(observed).not.toBe(origin);
+		expect(observed.foo).toBe(1);
+	});
 
-  it('isReactive', () => {
-    const origin = { foo: 1 }
-    const readonlyObj = readonly({ foo: 2 })
-    const reactiveObj = reactive({foo:2})
-    expect(isReactive(origin)).toBe(false)
-    expect(isReactive(readonlyObj)).toBe(false)
-    expect(isReactive(reactiveObj)).toBe(true)
-  });
+	it('isReactive', () => {
+		const origin = { foo: 1 };
+		const readonlyObj = readonly({ foo: 2 });
+		const reactiveObj = reactive({ foo: 2 });
+		expect(isReactive(origin)).toBe(false);
+		expect(isReactive(readonlyObj)).toBe(false);
+		expect(isReactive(reactiveObj)).toBe(true);
+	});
 
-  it('nested reative', () => {
-    const observed = reactive({ obj: { foo: 1 }, array: [1, 2, 3] })
-    expect(isReactive(observed.obj)).toBe(true)
-    expect(isReactive(observed.array)).toBe(true)
-  });
+	it('nested reative', () => {
+		const observed = reactive({ obj: { foo: 1 }, array: [1, 2, 3] });
+		expect(isReactive(observed.obj)).toBe(true);
+		expect(isReactive(observed.array)).toBe(true);
+	});
 
-  it('isProxy ', () => {
-    const readonlyObj = readonly({ foo: 1 })
-    const reactiveObj = reactive({ foo: 1 })
-    expect(isProxy(readonlyObj)).toBe(true)
-    expect(isProxy(reactiveObj)).toBe(true)
-  });
-})
+	it('isProxy ', () => {
+		const readonlyObj = readonly({ foo: 1 });
+		const reactiveObj = reactive({ foo: 1 });
+		expect(isProxy(readonlyObj)).toBe(true);
+		expect(isProxy(reactiveObj)).toBe(true);
+	});
+});
